178b889fcd4d0eb4fd879b2658d9f480bf45c639
Bram De Block <bram.de.block@skyline.be>
2023-03-28 14:15:10 +0200

    Add project files.

---
+++ SkylineEmpower_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SkylineEmpower_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("SkylineEmpower_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("D6348B81-A859-4A36-8487-A49921663F6B")]
+[assembly: AssemblyVersion("1.0.0.0")]
